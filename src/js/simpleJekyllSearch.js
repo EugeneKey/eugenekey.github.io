f23ff06ae09c57de0365d498c9ad61ccef5ceff0
EugeneKey <evgeny.konyaev@gmail.com>
2017-03-30 17:18:12 +0700

    search in decription

--- src/js/simpleJekyllSearch.js
+++ src/js/simpleJekyllSearch.js
@@ -2,7 +2,7 @@
     $.fn.simpleJekyllSearch = function(options) {
         var settings = $.extend({
             jsonFile        : '/search.json',
-            jsonFormat      : 'title,category,desc,url,date,shortdate',
+            jsonFormat      : 'title,description,category,desc,url,date,shortdate',
             template : '<li><article><a href="{url}">{title} <span class="entry-date"><time datetime="{date}">{date}</time></span></a></article></li>',
             searchResults   : '.search-results',
             searchResultsTitle   : '<h4>Search Results:</h4>',
